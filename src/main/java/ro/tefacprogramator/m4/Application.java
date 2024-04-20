426c74307fe93f4f5a144a47251c7f454951ae63
Florina Ene <eneflorina283@gmail.com>
2024-04-20 11:33:26 +0300

    fixed bug

--- src/main/java/ro/tefacprogramator/m4/Application.java
+++ src/main/java/ro/tefacprogramator/m4/Application.java
@@ -5,7 +5,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("New program");
 
-        int rezPow = Calculator.add(5, 4);
+        double rezPow = Calculator.pow(5, 4);
         System.out.println("rez pow = " + rezPow);
 
         int rezAdd = Calculator.add(100, 45);
@@ -14,10 +14,10 @@ public class Application {
         int rezSub = Calculator.substract(100, 45);
         System.out.println("rez subtract = " + rezSub);
 
-        int rezDiv = Calculator.substract(100, 45);
+        double rezDiv = Calculator.devide(100, 45);
         System.out.println("rez divide = " + rezDiv);
 
-        int rezMul = Calculator.substract(100, 45);
+        int rezMul = Calculator.multiply(56, 7);
         System.out.println("rez multiply = " + rezMul);
     }
 }
