9e677f0fdb93902a61a22a0c79a3cfe4a2c98260
Florina Ene <eneflorina283@gmail.com>
2024-04-20 11:03:47 +0300

    Added the divide method

--- src/main/java/ro/tefacprogramator/m4/Calculator.java
+++ src/main/java/ro/tefacprogramator/m4/Calculator.java
@@ -9,4 +9,8 @@ public class Calculator {
     public static int substract(int a, int b) {
         return a - b;
     }
+
+    public static double devide(double a, double b) {
+        return a / b;
+    }
 }
